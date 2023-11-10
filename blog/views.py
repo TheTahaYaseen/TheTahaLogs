8baed2a9f703ffd2681a9e5805d29356e6a69634
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-10 13:48:25 +0500

    Views & Urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register_view(request):
+    context = {}
+    return render(request, "templates/blog/auth/register.html", context)
+
+def login_view(request):
+    context = {}
+    return render(request, "templates/blog/auth/login.html", context)
