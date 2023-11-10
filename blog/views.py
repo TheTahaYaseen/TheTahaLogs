bec68f63828af6d96ad93cf660790de3740e3e18
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-10 13:53:52 +0500

    Fixed TemplatePath

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def register_view(request):
     context = {}
-    return render(request, "templates/blog/auth/register.html", context)
+    return render(request, "blog/auth/register.html", context)
 
 def login_view(request):
     context = {}
-    return render(request, "templates/blog/auth/login.html", context)
+    return render(request, "blog/auth/login.html", context)
